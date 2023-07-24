ea3080e0510963b20db79fae67d1c308b71b7fc7
Hacker-Wolf <alex.krysiuk@icloud.com>
2023-07-24 17:10:00 +0200

    Backend server architecture

--- BuberBreakfast/Program.cs
+++ BuberBreakfast/Program.cs
@@ -1,15 +1,25 @@
-using BuberBreakfast.Services.Breakfasts;
-
 var builder = WebApplication.CreateBuilder(args);
-{
-    builder.Services.AddControllers();
-    builder.Services.AddScoped<IBreakfastService, BreakfastService>();
-}
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler("/error");
-    app.UseHttpsRedirection();
-    app.MapControllers();
-    app.Run();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
